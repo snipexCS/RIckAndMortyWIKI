2e6b8930ccb6cce2856e1c5a33bf9cbb4d55a812
Shod <dilshod@niyaz@gmail.com>
2025-11-10 16:00:03 +0800

    todo lists adding delete function , edit function should be fixed

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,7 +2,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
   plugins: [react()],
+  plugins: ['babel-plugin-react-compiler'],
   // add other config here if needed
 });
